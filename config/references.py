d8f72a5ea27c05ff63742b8f771d045c59964348
Sofian Medbouhi <sofian.medbouhi@manakin.fr>
2017-08-03 18:14:46 +0200

    parameters updated

--- config/references.py
+++ config/references.py
@@ -8,13 +8,13 @@ MODEL_PATH      = os.path.join(BASE_PATH, 'data/models/')
 GRAPH_PATH      = os.path.join(BASE_PATH, 'graph/')
 
 # File containing data to be t-SNEed
-INPUT_FILE_BASE_NAME = 'one_hot'
+INPUT_FILE_BASE_NAME = 'onehot'
 
 # A version is a string added to the end of each filename
-VERSION = '_20170614'
+VERSION = '_20170728'
 
 # default RN for predictions
-DEFAULT_PREDICTOR = 'metapredict.npz'
+DEFAULT_PREDICTOR = 'metapredict'
 
 # data output name (labels)
 OUTPUT_NAME = 'account'
@@ -22,6 +22,7 @@ OUTPUT_NAME = 'account'
 # t-SNE parameters
 # best tuple so far is (50,1000,pca,15000)
 """
+
 PARAMS = { 'perplexities'  : [30,40,50],
            'learning_rates': [500, 800, 1000],
            'inits'         : ['random', 'pca'],
@@ -29,14 +30,14 @@ PARAMS = { 'perplexities'  : [30,40,50],
          }
 """
 
-PARAMS = { 'perplexities'  : [40],
+PARAMS = { 'perplexities'  : [40, 50, 60, 70, 80],
            'learning_rates': [1000],
-           'inits'         : ['pca'],
+           'inits'         : ['random'],
            'n_iters'       : [15000]
          }
 
 # 30 for OVH, 50 for local, 15 for epinal
-REDUCTION_SIZE_FACTOR = 3
+REDUCTION_SIZE_FACTOR = 1
 
 # False -> load representation
 # True  -> find new projection for t-SNE
