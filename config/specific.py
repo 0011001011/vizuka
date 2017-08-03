29976fab359d6cff29ac7e2529d05001e67a5ee9
Sofian Medbouhi <sofian.medbouhi@manakin.fr>
2017-08-03 18:16:47 +0200

    more config files

---
+++ config/specific.py
@@ -0,0 +1,18 @@
+COLUMNS_TO_SCATTER = [
+                'account',
+                'amount',
+                'operation_code',
+                'operation_date',
+                'bank_code',
+                'activity_start_date',
+                'ape_code',
+                'vat_regime',
+                'fiscal_regime',
+                'imposition_regime',
+                'bnc_option',
+                'legal_obligation',
+                'vat_type',
+                'vat_intra_flag',
+                'vat_class'
+                ]
+GRAPH_PATH = '/home/sofian/data/graph/'
