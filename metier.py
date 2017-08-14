eb535c1b19f4fb22204ffd570fb5be3e120d4316
Nicolas Rolin <nicolas.rolin@cubber.com>
2017-08-14 18:08:37 +0200

    flake8

--- metier.py
+++ metier.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
+
 class Annotations(pd.DataFrame):
     def __init__(self, array):
-        columns = [
-                'pk',
-                'amount',
-                'operation_code',
-                'operation_date',
-                'bank_wording',
-                'creditor_id',
-                'bank_code',
-                'ape_code',
-                'vat_regime',
-                'fiscal_regime',
-                'imposition_regime',
-                'bnc_option',
-                'legal_obligation',
-                'account',
-                'activity_start_date',
-                'libellé',
-                ]
+        columns = ['pk',
+                   'amount',
+                   'operation_code',
+                   'operation_date',
+                   'bank_wording',
+                   'creditor_id',
+                   'bank_code',
+                   'ape_code',
+                   'vat_regime',
+                   'fiscal_regime',
+                   'imposition_regime',
+                   'bnc_option',
+                   'legal_obligation',
+                   'account',
+                   'activity_start_date',
+                   'libellé',
+                   ]
         super(Annotations, self).__init__(array, columns=columns)
