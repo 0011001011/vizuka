62c73b6be0a77e7c11940b25c34814480b470fcd
Sofian Medbouhi <sofian.medbouhi@manakin.fr>
2017-08-24 11:07:34 +0200

    correct relative imports

--- data_viz/launch_viz.py
+++ data_viz/launch_viz.py
@@ -12,9 +12,9 @@ import os
 
 matplotlib.use('Qt5Agg')  # noqa
 
-from data_viz import dim_reduction
-from data_viz import labelling
-from data_viz import vizualization
+import dim_reduction
+import labelling
+import vizualization
 
 
 import argparse
