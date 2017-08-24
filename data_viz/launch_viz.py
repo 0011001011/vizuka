f07bb2af8e4700ca6c3b8c58976729abc4739e90
Sofian Medbouhi <sofian.medbouhi@manakin.fr>
2017-08-24 11:00:44 +0200

    syntax error post merge

--- data_viz/launch_viz.py
+++ data_viz/launch_viz.py
@@ -138,7 +138,7 @@ def main():
             class_decoder=class_decoder,
             class_encoder=class_encoder,
             special_class='0',
-            number_of_clusters=120
+            number_of_clusters=120,
             output_path   = os.path.join(os.path.__file__, 'output.csv'),
             model_path    = MODEL_PATH,
         )
