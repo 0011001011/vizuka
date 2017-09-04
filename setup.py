1ec4860009c5d5c1100821d6155ce0989b25ec2e
Sofian Medbouhi <sofian.medbouhi@manakin.fr>
2017-09-04 15:32:30 +0200

    renaming in  setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 import os
 
 setup(
-        name='Data-viz',
-        version='0.13',
-        packages=['data_viz/'],
+        name='Vizuka',
+        version='0.14',
+        packages=['vizuka/'],
         entry_points = {
-            'console_scripts':['vizuka=data_viz.launch_viz:main'],
+            'console_scripts':['vizuka=vizuka.launch_viz:main'],
             },
         description='Represents your high-dimensional datas in a 2D space and play wih it',
         long_description = open(os.path.join(os.path.dirname(__file__),'README.md')).read(),
-        install_requires = open('data_viz/requirements/requirements.txt').read(),
+        install_requires = open('vizuka/requirements/requirements.txt').read(),
         license = 'GPL V3',
         author='Sofian Medbouhi',
         author_email='sof.m.sk@free.fr',
