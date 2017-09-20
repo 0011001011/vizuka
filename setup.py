24f559919f6f62303268a450833b74075ec49efc
Sofian Medbouhi <sofian.medbouhi@manakin.fr>
2017-09-20 16:08:17 +0200

    stable version deserves a new version numer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
         name='Vizuka',
-        version='0.25.dev13',
+        version='0.25',
         packages=['vizuka/'],
         entry_points = {
             'console_scripts':['vizuka=vizuka.launch_viz:main'],
