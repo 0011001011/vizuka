fe67273c24974c6a5b3c419f7be0d3c875417afb
Sofian Medbouhi <sofian.medbouhi@manakin.fr>
2017-10-16 12:24:42 +0200

    new version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ data = {
 
 setup(
         name='Vizuka',
-        version='0.27.12',
+        version='0.27.13',
         packages=find_packages(),#['vizuka/'],
         package_data = data,
         entry_points = {
